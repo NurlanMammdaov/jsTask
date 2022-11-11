df4666f900c293746cb1ebb00962cc540c7b8cc0
Nurlan Memmedov <nurlan.memmedov8818@gmail.com>
2022-11-11 05:46:12 +0400

    Js Task HAzirdir

---
+++ main.js
@@ -0,0 +1,49 @@
+//Task1
+// var background = document.getElementById("bigBackground");
+// var blue = document.getElementById("div1")
+// var red = document.getElementById("div2")
+// var green = document.getElementById("div3")
+// blue.addEventListener("click", function () {
+//     background.style.backgroundColor = "blue"
+// });
+// red.addEventListener("click", function () {
+//     background.style.backgroundColor = "red"
+// });
+// green.addEventListener("click", function () {
+//     background.style.backgroundColor = "green"
+// });
+
+//Task2
+// var num1 = document.getElementById("input1")
+// var num2 = document.getElementById("input2")
+// var num3 = document.getElementById("input3")
+// var sum = document.getElementById("hesabla")
+// var count = 0;
+// sum.addEventListener("click",function(){
+//     switch(num3.value){
+//         case '+':
+//             count = parseInt(num1.value) + parseInt(num2.value)
+//             console.log(count);
+//             break;
+//             case '-':
+//                 count = parseInt(num1.value) - parseInt(num2.value)
+//                 console.log(count);
+//                 break;
+//                 case '*':
+//                     count = parseInt(num1.value) * parseInt(num2.value)
+//                     console.log(count);
+//                     break;
+//                     case '/':
+//                         if(num2.value == 0){
+//                             console.log("0 a bolune bilmez")
+//                             break;
+//                         }
+//                         else{
+//                             count = parseInt(num1.value) / parseInt(num2.value)
+//                             console.log(count);
+//                             break;
+//                         }
+                        
+//         }
+        
+// })
